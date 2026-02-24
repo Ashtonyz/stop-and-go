fd3c47f9f41f050ff247d0c04a9294457fbd827b
Ashtonyz <ST10510941@myemeris.edu.za>
2026-02-24 12:34:13 +0200

    Stop and go

---
+++ src/introtojava/IntroTOjava.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package introtojava;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class IntroTOjava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*im gonna code now*/
+        System.out.println("   ********         XXXXXXXX   \n"
+                + " *          *     X          X \n*  S T O P   *   X     GO     X");
+        System.out.println(" *          *     X          X");
+        System.out.println("   ********         XXXXXXXX");
+        System.out.println("      **               XX\n      **               XX"
+                + "\n      **               XX\n      **               XX");
+       /**
+        * welcome to my first application
+        */
+        System.out.println("     ****             XXXX\n  **********       XXXXXXXXXX");
+        System.out.println("|-----------------------------|\n"
+                + "|-----------------------------|");
+       //hi im trying to code       
+    }
+}
